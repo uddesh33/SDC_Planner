a574e49292b4732afb38d1ab04a84a98fde92666
Tejas <tejas.darwai@volkswagen.co.in>
2023-04-06 10:32:47 +0530

    Initial commit

---
+++ src/main/java/com/vwg/vw/sdc_planner/SdcPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.vwg.vw.sdc_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdcPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vwg/vw/sdc_planner/SdcPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwg.vw.sdc_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdcPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
